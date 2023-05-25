47439182ba41f5b577a8441f320a53ea4e415134
mamamamamaa <maks.denishchuk@gmail.com>
2023-05-25 08:52:53 +0300

    add layout styles

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,6 +1,7 @@
 import { FC, ReactNode } from "react";
 import { Footer } from "@/components/Footer/Footer";
 import { Header } from "@/components/Header/Header";
+import style from "./Layout.module.css";
 
 interface Props {
   children: ReactNode;
@@ -8,9 +9,9 @@ interface Props {
 
 export const Layout: FC<Props> = ({ children }) => {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className={style.layout}>
       <Header />
-      <main className="flex-grow">{children}</main>
+      <main className={style.main}>{children}</main>
       <Footer />
     </div>
   );
