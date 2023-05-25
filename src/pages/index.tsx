0b5080ac59b60c31095891d4444260a6b91dee12
mamamamamaa <maks.denishchuk@gmail.com>
2023-05-25 08:58:33 +0300

    add home page styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,15 @@
 import Link from "next/link";
+import style from "./Home.module.css";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-4xl font-bold text-center mb-4">
-        Welcome to ElifTech Delivery!
-      </h1>
-      <p className="text-lg text-center mb-8">
+    <div className={style.pageContainer}>
+      <h1 className={style.welcomeHeader}>Welcome to ElifTech Delivery!</h1>
+      <p className={style.paragraph}>
         We are a leading food delivery company, providing delicious meals right
         to your doorstep.
       </p>
-      <Link
-        href="/shot"
-        className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded"
-      >
+      <Link href="/shot" className={style.orderLink}>
         Order Now
       </Link>
     </div>
