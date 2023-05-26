e2272e70b7b7aa09e96c09a173f13a5b70c865c8
mamamamamaa <maks.denishchuk@gmail.com>
2023-05-26 09:55:56 +0300

    fix up history page

--- src/pages/History/History.tsx
+++ src/pages/History/History.tsx
@@ -2,17 +2,18 @@ import { HistoryForm } from "../../components/HistoryForm/HistoryForm.tsx";
 import { OrderHistoryList } from "../../components/OrderHistoryList/OrderHistoryList.tsx";
 import { useAppSelector } from "../../redux/hooks.ts";
 import { selectOrderHistory } from "../../redux/selectors.ts";
+import store from "./History.module.css";
 
 export default function History() {
   const orderHistory = useAppSelector(selectOrderHistory);
   return (
     <>
-      <div className="flex flex-col justify-center gap-10 w-full">
-        <section className="border-2 rounded-xl py-7 px-5">
+      <div className={store.pageWrapper}>
+        <section className={store.historyFormWrapper}>
           <HistoryForm />
         </section>
         {orderHistory.length > 0 && (
-          <section className="border-2 rounded-xl py-7 px-5 overflow-y-scroll">
+          <section className={store.historyListWrapper}>
             <OrderHistoryList orderHistory={orderHistory} />
           </section>
         )}
