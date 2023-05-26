f4874501f38f4b3612d5d0aebd2b53b23ec8e6c0
mamamamamaa <maks.denishchuk@gmail.com>
2023-05-26 10:07:45 +0300

    fix up spinner

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -1,11 +1,12 @@
 import { FaCircleNotch } from "react-icons/fa";
+import style from "./Spinner.module.css";
 
 export const SpinnerLoader = () => {
   return (
-    <div className="fixed inset-0 flex items-center justify-center z-50">
-      <div className="absolute inset-0 bg-gray-900 opacity-50"></div>
-      <div className="flex items-center justify-center bg-white rounded-lg p-4">
-        <FaCircleNotch className="animate-spin text-blue-500 text-4xl" />
+    <div className={style.spinnerWrapper}>
+      <div className={style.backdrop}></div>
+      <div className={style.iconWrapper}>
+        <FaCircleNotch className={style.icon} />
       </div>
     </div>
   );
