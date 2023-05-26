764a538bfc65b3a76d4afe3c6b2318ac41bdc5aa
mamamamamaa <maks.denishchuk@gmail.com>
2023-05-26 10:19:42 +0300

    fix up products

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -2,6 +2,7 @@ import { FC } from "react";
 import { useAppSelector } from "../../redux/hooks.ts";
 import { selectCart } from "../../redux/selectors.ts";
 import noImage from "../../../public/no_image.gif";
+import style from "./ProductCard.module.css";
 
 interface Props {
   name: string;
@@ -21,20 +22,19 @@ export const ProductCard: FC<Props> = ({
   const isAlreadyInCart = Boolean(cart.find(({ _id }) => id === _id));
 
   return (
-    <section className="flex flex-col justify-between bg-white rounded-lg shadow p-4 h-full">
-      <img src={image || noImage} alt="Product" className="w-full" />
+    <section className={style.cardWrapper}>
+      <img src={image || noImage} alt="Product" className={style.img} />
       <div>
-        <h3 className="text-lg font-bold mt-2">{name}</h3>
+        <h3 className={style.productName}>{name}</h3>
         <button
           type="button"
-          className="h-7 w-full rounded py-1 px-2 bg-blue-600 text-white text-sm border border-blue-600 hover:text-blue-600 hover:bg-transparent disabled:bg-transparent disabled:text-blue-600"
+          className={style.orderButton + " bg-blue-600"}
           disabled={isAlreadyInCart}
           onClick={() => handleAddToCard(id)}
         >
           Order
         </button>
       </div>
-      <button type="button"></button>
     </section>
   );
 };
--- src/components/ProductsList/ProductsList.tsx
+++ src/components/ProductsList/ProductsList.tsx
@@ -3,6 +3,7 @@ import { ProductCard } from "../ProductCard/ProductCard.tsx";
 import { useAppDispatch } from "../../redux/hooks.ts";
 import { setInCart } from "../../redux/features/productsSlice.ts";
 import { IProduct } from "../../types/store.intarface.ts";
+import style from "./ProductsList.module.css";
 
 interface Props {
   storeId: string;
@@ -17,7 +18,7 @@ export const ProductsList: FC<Props> = ({ storeId, activeProducts }) => {
   return (
     <>
       {activeProducts && (
-        <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <ul className={style.productsList}>
           {activeProducts.map(({ _id, name, image }) => (
             <li key={_id}>
               <ProductCard
