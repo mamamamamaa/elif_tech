d26f0404ca13a6f4c67e22d391ba29ae15e60018
mamamamamaa <maks.denishchuk@gmail.com>
2023-05-26 09:59:37 +0300

    fix up cart page

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -1,14 +1,15 @@
 import { OrderForm } from "../../components/OrderForm/OrderForm.tsx";
 import { CartList } from "../../components/CartList/CartList.tsx";
+import style from "./Cart.module.css";
 
 export default function Cart() {
   return (
     <>
-      <div className="flex flex-col justify-center md:flex-row gap-10 w-full h-full">
-        <section className="md:w-2/4 md:h-auto border-2 rounded-xl py-7 px-5">
+      <div className={style.pageWrapper}>
+        <section className={style.orderFormWrapper}>
           <OrderForm />
         </section>
-        <section className="md:w-2/4 border-2 rounded-xl py-7 px-5 overflow-y-scroll">
+        <section className={style.orderListWrapper}>
           <CartList />
         </section>
       </div>
